e7f85eeeb089aa276c465951523682eaabfda26a
Habced <seungyboy@gmail.com>
2020-09-03 11:16:18 +0900

    Reworked

--- backend/msc_backend/msc_backend/serializers.py
+++
@@ -1,5 +0,0 @@
-from django.utils.translation import ugettext_lazy as _
-
-from rest_framework import serializers
-from django.contrib.auth.hashers import make_password
-from django.conf import settings
