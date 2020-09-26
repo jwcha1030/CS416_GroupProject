7166e414e52d4bc1adfd4cca55196b63a0838412
timkim0713 <timkim0713@gmail.com>
2020-09-26 15:05:03 +0900

    main page carousel

--- frontend/src/components/pages/HomePage/Data.js
+++ frontend/src/components/pages/HomePage/Data.js
@@ -1,4 +1,30 @@
-export const temp1 = {
+export const collections = {
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "MSC Collections",
+  headline: "Discover new products we made!",
+  description: "Website is in progress",
+  buttonLabel: "",
+  imgStart: "",
+  img: "",
+  alt: "",
+};
+
+export const ourteam = {
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "MSC Our Team",
+  headline: "Meet our members",
+  description: "Website is in progress",
+  buttonLabel: "",
+  imgStart: "",
+  img: "",
+  alt: "",
+};
+
+export const aboutus = {
   lightBg: false,
   lightText: true,
   lightTextDesc: true,
--- frontend/src/components/pages/HomePage/Home.js
+++ frontend/src/components/pages/HomePage/Home.js
@@ -1,12 +1,46 @@
 import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+import "bootstrap/dist/css/bootstrap.min.css";
 import HeroSection from "../../HeroSection";
-import { temp1, temp2, temp3, temp4 } from "./Data";
+import { collections, ourteam, aboutus } from "./Data";
+import msc_banner from "../../../images/msc_banner.JPG";
+import msc_banner2 from "../../../images/6.png";
+import msc_banner3 from "../../../images/7.png";
 
 function Home() {
   return (
-    <>
-      <HeroSection {...temp1} />
-    </>
+    <div>
+      <Carousel>
+        <Carousel.Item>
+          <img src={msc_banner} alt="First slide" />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img className="d-block" src={msc_banner2} alt="Second slide" />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img className="d-block" src={msc_banner3} alt="Thrid slide" />
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+      <HeroSection {...collections} />
+
+      <HeroSection {...ourteam} />
+
+      <HeroSection {...aboutus} />
+    </div>
   );
 }
 
