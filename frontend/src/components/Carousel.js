28434e28ea21c4d13c0e02e5a3bad4342ac82684
Daekyung Kim <timkim0713@gmail.com>
2020-09-29 01:09:22 +0900

    carousel position in mobile view fixed

--- frontend/src/components/Carousel.js
+++ frontend/src/components/Carousel.js
@@ -10,7 +10,7 @@ const renderCarousel = (item, index) => {
 
     return (
         <Carousel.Item  >
-            <img className="" src={item.img} alt={item.alt} />
+            <img className="carousel-image" src={item.img} alt={item.alt} />
             <Carousel.Caption >
                 <h3>{item.caption}</h3>
                 <p>{item.description}</p>
