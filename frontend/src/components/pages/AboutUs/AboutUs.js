8a5c8297b3e4325da33c09c08b80195272deb2fe
timkim0713 <timkim0713@gmail.com>
2020-09-29 16:12:55 +0900

    about us hero/img

--- frontend/src/components/pages/AboutUs/AboutUs.js
+++ frontend/src/components/pages/AboutUs/AboutUs.js
@@ -1,15 +1,17 @@
 import React from "react";
 import HeroSection from "../../HeroSection";
+import about_us from "../../../images/aboutus.jpg";
+
 import { aboutUs, virtualStore } from "./Data";
-import { AnimatePresence, motion } from "framer-motion"
+import { AnimatePresence, motion } from "framer-motion";
 
 function AboutUs() {
   return (
     <motion.div
-    initial={{opacity:0}}
-    animate={{opacity:1}}
-    exit={{opacity:0}} >
-      
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
       <HeroSection {...aboutUs}></HeroSection>
       <HeroSection {...virtualStore} />
     </motion.div>
--- frontend/src/components/pages/AboutUs/Data.js
+++ frontend/src/components/pages/AboutUs/Data.js
@@ -1,3 +1,7 @@
+import model_orange from "../../../images/model_orange.jpg";
+import about_us from "../../../images/aboutus.jpg";
+import shop from "../../../images/shop.jpg";
+
 export const aboutUs = {
   lightBg: false,
   lightText: true,
@@ -8,19 +12,19 @@ export const aboutUs = {
     "The Merchandising Society Club (MSC) makes Fashion Institute of Technology (FIT) and StonyBrook University merchandise. Their goal is to not only produce merchandise for students andfaculties but also to showcase and sell their products that are crafted after extensive research onfashion trends and sales planning.Thus, to display their products for sale, MSC runs its own physical Retail Revolution Store at FIT,and they are currently in need of expanding their collection online. Particularly during the COVID 19crisis, customers cannot regularly access the offline retail store. Thus, having an online means ofaccessing MSC is a massive benefit for them.",
   buttonLabel: "Make an Inquiry",
   imgStart: "",
-  img: "",
+  img: about_us,
   alt: "MSC Main Image",
 };
 
 export const virtualStore = {
-  lightBg: true,
-  lightText: false,
-  lightTextDesc: false,
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
   topLine: "Retail Revolution Store",
   headline: "Experience Our Store Online",
   description: "Virtually experience Retail Revolution Store",
   buttonLabel: "View Store",
   imgStart: "",
-  img: "",
+  img: shop,
   alt: "Virtual Retail Revolution Shop",
 };
