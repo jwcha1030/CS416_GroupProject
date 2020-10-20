de31e6ef25a3ecd0273bf773aee22b462f3756b4
timkim0713 <timkim0713@gmail.com>
2020-10-21 01:29:16 +0900

    VR transportation function added

--- frontend/src/components/vr.js
+++ frontend/src/components/vr.js
@@ -1,40 +1,72 @@
-import React, { Component } from 'react';
+import React, { Component,useState, useEffect  } from 'react';
 import { render } from 'react-dom';
- 
+import "./VR.css";
+
 import { Pannellum, PannellumVideo } from "pannellum-react";
 import myImage from "../images/vr_sample.jpg";
+import myImage2 from "../images/vr_sample2.jpg";
+
 // import myVideo from "./images/video.mp4";
  
- 
-const vr = () => (
-  <div>
-     <Pannellum
-        width="100%"
-        height="800px"
-        image={myImage}
+
+function VR () {
+  const [currentVR, setNewVR] = useState(myImage2);
+
+  const handleClickHotSpot =(evt,newImage)=>{
+      setNewVR(newImage)
+  }
+return(
+<div>
+      <Pannellum
+        width="800px"
+        height="400px"
+        image={currentVR}
         pitch={10}
-        yaw={180}
-        hfov={110}
+        yaw={400}
+        hfov={100}
         autoLoad
-        onLoad={() => {
-            console.log("panorama loaded");
-        }}
+        author=""
+        title=""
+        orientationOnByDefault={false}
+        draggable
+        keyboardZoom
+        mouseZoom
+        preview=""      
+        previewAuthor=""
+        previewTitle="Retail Revolution Shop"
+        showControls
+        showFullscreenCtrl
+        showZoomCtrl
+        onLoad={()=>{console.log("panorama loaded");}}
+        onScenechange={(id)=>{console.log("Scene has change on " + id);}}
+        onScenechangefadedone={()=>{console.log("panorama loaded");}}
+        onError={(err)=>{console.log("Error" , err);}}
+        onErrorcleared={()=>{console.log("Error Cleared");}}
+        onMousedown={(evt)=>{console.log("Mouse Down" , evt);}}
+        onMouseup={(evt)=>{console.log("Mouse Up", evt);}}
+        onTouchstart={(evt)=>{console.log("Touch Start", evt);}}
+        onTouchend={(evt)=>{console.log("Touch End", evt);}}
+        hotspotDebug={false}
     >
-      <Pannellum.Hotspot
+        <Pannellum.Hotspot 
         type="info"
         pitch={11}
         yaw={-167}
         text="Info Hotspot Text 3"
-        URL=""
-      />
- 
-      <Pannellum.Hotspot
-        type="info"
+        URL="https://github.com/farminf"
+        />
+
+        <Pannellum.Hotspot 
+        type="custom"
+        name="shoes"
         pitch={31}
-        yaw={-107}
-        text="Info Hotspot Text 4"
-        URL="https://github.com/farminf/pannellum-react"
-      />
+        yaw={150}
+
+        handleClick={(evt , args) => handleClickHotSpot(evt , args.name)}
+        handleClickArg={{ "name":myImage }}
+
+/>
+
     </Pannellum>
  
     {/******  for video 360 component *******/}
@@ -67,7 +99,7 @@ const vr = () => (
       />
     </PannellumVideo> */}
  
-    </div>
-);
+    </div>);
+}
  
-export default vr;
+export default VR;
