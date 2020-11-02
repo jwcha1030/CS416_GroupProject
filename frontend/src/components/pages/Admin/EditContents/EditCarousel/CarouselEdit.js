4ff8867e4750f459ede13b8c89a5be14a323b54a
jwcha1030 <42469606+jwcha1030@users.noreply.github.com>
2020-11-03 05:12:47 +0900

    Tried POST, PUT, DELETE, GET but only DELETE and GET works; Currently res_code=0 for some unkown reason

--- frontend/src/components/pages/Admin/EditContents/EditCarousel/CarouselEdit.js
+++ frontend/src/components/pages/Admin/EditContents/EditCarousel/CarouselEdit.js
@@ -1,5 +1,4 @@
-import React, {useState} from 'react';
-import {CarouselData} from '../../../HomePage/CarouselData';
+import React, {useEffect, useRef, useState} from 'react';
 import DataTable from "../DataTable";
 import {Button} from "../../../../Button";
 import Modal from "react-bootstrap/Modal";
@@ -7,47 +6,53 @@ import CarouselForm from "./CreateCarouselForm"
 import {Link} from "react-router-dom";
 import {IoIosArrowBack} from "react-icons/io";
 
-export default function CarouselEdit(prop) {
-	
+export default function CarouselEdit() {
 
-	var axios = require('axios')
-
-	
-	// TODO IDK WHERE TO PUT THIS
-	// put this in a constructor or something to get 
-	axios.get('http://127.0.0.1:8000/home_page_carousel/get_all/',)
-	.then(function (response) {
-		// console.log(response.data);
-		// Check if internet connection was working
-		if(response.status == 200){
-			if(response.data.res_code == 1){
-				// Everything worked correctly
-				// Do something with the returned data
-
-			// } else if (){
-				// Check other res_code with else if
-			// }
-			} else {
-				// Unhandled res_code
-			}
-		} else {
-			// TODO handle unable to connect with database
-		}
-	})
-	.catch(function (error) {
-		// TODO handle error with the call
-		console.log(error);
-	});
-	
-	const [current_item, setItem] = useState(CarouselData[0]);
-  const [data, setData] = useState(CarouselData);
+  const [data,setData]=useState([]);
+  const [current_item, setItem] = useState(null); //object
   const [showCreateModal, setCreateModalShow] = useState(false);
   const [showEditModal, setEditModalShow] = useState(false);
-
   const [newCaption, setNewCaption] = useState('');
   const [newDesc, setNewDesc] = useState('');
-  const [newImg, setNewImg] = useState('');
-  const headers = Object.keys(CarouselData[0]);
+  const [newImg, setNewImg] = useState(null); //file object
+
+  //['id', 'img', 'idx', 'caption','desc']
+  const [headers, setHeaders] = useState([]); //table headers
+  let axios = require('axios');
+  //initial fetch
+  const fetchAllData = ()=>{
+    axios.get("https://sunyk-msc-backend.herokuapp.com/home_page_carousel/get_all/")
+      .then(response =>{
+        // console.log(response.data);
+        // Check if internet connection was working
+        if(response.status === 200){
+          if(response.data.res_code === 1){
+            // Everything worked correctly
+            // Do something with the returned data
+            // console.log(response.data.results);
+            setData(response.data.results);
+            setHeaders(Object.keys(response.data.results[0]));
+            console.log("fetch complete");
+            // } else if (){
+            // Check other res_code with else if
+            // }
+          } else {
+            // Unhandled res_code
+            alert("Fetch Data: Unhandled res_code");
+          }
+        } else {
+          // TODO handle unable to connect with database
+          alert("Fetch: Unable to connect with database");
+        }
+      })
+      .catch(function (error) {
+        // TODO handle error with the call
+        alert("Fetch: Call error");
+        console.log(error);
+      });
+  };
+  useEffect(()=>{fetchAllData()},[]); //fetch once
+
 
   const handleCaption = (e) => {
     setNewCaption(e.target.value);
@@ -57,14 +62,43 @@ export default function CarouselEdit(prop) {
   };
   const handleImg = (e) => {
 		// Changed from e.target.value to e.target.files[0] because the api needs a file
-    setNewImg(e.target.files[0] );
+    // console.log(e.target.files[0].name);
+    setNewImg(e.target.files[0]);
   };
 
   const handleDelete = (id) => {
-    const removedItems = [...data].filter(item => item.id !== id);
 
-    setData(removedItems);
+    axios.delete("https://sunyk-msc-backend.herokuapp.com/home_page_carousel/del/"+id+"/")
+      .then(response =>{
+        // console.log(response.data);
+        // Check if internet connection was working
+        if(response.status === 200){
+          if(response.data.res_code === 1){
+            // Everything worked correctly
+            // Do something with the returned data
+            // console.log(response.data.results);
+            console.log("Delete SUCCESS");
+            const removedItems = [...data].filter(item => item.id !== id);
+            setData(removedItems);
+            // } else if (){
+            // Check other res_code with else if
+            // }
+          } else {
+            // Unhandled res_code
+            alert("Delete: Unhandled res_code");
+          }
+        } else {
+          // TODO handle unable to connect with database
+          alert("Delete: Unable to connect with database");
+        }
+      })
+      .catch(function (error) {
+        // TODO handle error with the call
+        alert("Delete: Call error");
+        console.log(error);
+      });
   };
+
   //edit modal handler
   const changeItem = (id) => {
     const currentItem = (data.filter(item => item.id === id))[0]; //selects the item that is clicked
@@ -77,13 +111,60 @@ export default function CarouselEdit(prop) {
     setEditModalShow(false);
   };
   const handleEditSubmit = (e) => {
+    let itemID = current_item.id;
     e.preventDefault();
     let i;
-    for(i=0; i<data.length; i++){
-      if(data[i].id===current_item.id){
+    for (i = 0; i < data.length; i++) {
+      if (data[i].id === itemID) {
         break;
       }
     }
+    // console.log("Current Item ID:",itemID);
+    let apiBaseUrl = "https://sunyk-msc-backend.herokuapp.com/home_page_carousel/edit/" + itemID + "/";
+    const formData = new FormData();
+    let inputImg = newImg == null ? data[i].img : newImg;
+    let inputCap = newCaption === '' ? data[i].caption : newCaption;
+    let inputDesc = newDesc === '' ? data[i].desc : newDesc;
+    formData.append('img', inputImg);
+    formData.append('caption', inputCap);
+    formData.append('desc', inputDesc);
+    axios.put(apiBaseUrl, formData).then(response=>{
+      // Check if internet connection was working
+      if(response.status === 200){
+        alert("PUT res_code:",response.data.res_code);
+        if(response.data.res_code === 1){
+          // Everything worked correctly
+          // Do something with the returned data
+          console.log("Put SUCCESS",response.data.home_page_carousel);
+          //update frontend
+          let newData = [...data];
+          newData[i].caption = inputCap;
+          newData[i].desc = inputDesc;
+          newData[i].img = inputImg;
+          setData(newData);
+          // } else if (){
+          // Check other res_code with else if
+          // }
+        } else {
+          // Unhandled res_code
+          alert("Put: Unhandled res_code");
+        }
+      } else {
+        // TODO handle unable to connect with database
+        alert("Put: unable to connect with database");
+      }
+    }).catch(function (error) {
+      // TODO handle error with the call
+      alert("Put: Call error");
+      console.log(error);
+    });
+
+    //reinitialize
+    setNewCaption('');
+    setNewDesc('');
+    setNewImg(null);
+/*
+    //This part is only when frontend was working
     let newData = [...data];
     newData[i].caption = newCaption===''? data[i].caption:newCaption;
     setNewCaption(''); //reinitialize
@@ -92,77 +173,94 @@ export default function CarouselEdit(prop) {
     newData[i].img = newImg === '' ? data[i].img : newImg;
     setNewImg('');
     setData(newData);
+*/
     setEditModalShow(false);
   };
 
   //create modal handler
   const handleCreateSubmit = (e) => {
-
-    var apiBaseUrl = "https://sunyk-msc-backend.herokuapp.com/home_page_carousel/add/";
+    let apiBaseUrl = "https://sunyk-msc-backend.herokuapp.com/home_page_carousel/add/";
     // var apiBaseUrl = "http://127.0.0.1:8000/home_page_carousel/add/";
-		
 
-		// if the data does not include a File (eg img) use a json 
+		// if the data does not include a File (eg img) use a json
     // var payload = {
     //   caption: newCaption,
 		// 	desc: newDesc,
 		// 	img: newImg
 		// };
-		
 		// if the new data includes a File (eg img) use FromData();
-		const formData = new FormData();
-		formData.append('img', newImg);
-		// Wait for API to include caption and description
-		// formData.append('caption', newCaption);
-		// formData.append('desc', newDesc);
-
-		axios.post(apiBaseUrl, formData )
-			.then( function (response) {
-				// console.log(response);
-				// Check if internet connection was working
-				if(response.status == 200){
-					if(response.data.res_code == 1){
-						// Everything worked correctly
-						// Do something with the returned data
-
-					// } else if (){
-						// Check other res_code with else if
-					// }
-					} else {
-						// Unhandled res_code
-					}
-				} else {
-					// TODO handle unable to connect with database
-				}
-			})
-		.catch(function (error) {
-			// TODO handle error with the call
-			console.log(error);
-		});
-
-    e.preventDefault();
-    let maxID = 0;
-    for(let i=0; i<data.length; i++){
-      if(maxID<data[i].id) {
-        maxID=data[i].id;
-      }
+    console.log("Post",newImg);
+    if(newImg){
+      const formData = new FormData();
+      formData.append('img', newImg);
+      formData.append('caption', newCaption);
+      formData.append('desc', newDesc);
+      axios.post(apiBaseUrl, formData ).then(response=>{
+        // Check if internet connection was working
+        if(response.status === 200){
+          alert("POST res_code:",response.data.res_code);
+          if(response.data.res_code === 1){
+            // Everything worked correctly
+            // Do something with the returned data
+            console.log("Post SUCCESS",response.data.home_page_carousel);
+            setData([...data,response.data.home_page_carousel]);
+            // } else if (){
+            // Check other res_code with else if
+            // }
+          } else {
+            // Unhandled res_code
+            alert("Post: Unhandled res_code");
+          }
+        } else {
+          // TODO handle unable to connect with database
+          alert("Post: unable to connect with database");
+        }
+      }).catch(function (error) {
+          // TODO handle error with the call
+          alert("Post: Call error");
+          console.log(error);
+        });
+    }else{
+      console.log("Create new slide without img");
+      let payload = {
+        caption: newCaption,
+        desc: newDesc,
+        img: null
+      };
+      axios.post(apiBaseUrl, payload).then(response=>{
+        // Check if internet connection was working
+        if(response.status === 200){
+          alert("POST res_code:",response.data.res_code);
+          if(response.data.res_code === 1){
+            // Everything worked correctly
+            // Do something with the returned data
+            console.log("Post SUCCESS",response.data.home_page_carousel);
+            setData([...data,response.data.home_page_carousel]);
+            // } else if (){
+            // Check other res_code with else if
+            // }
+          } else {
+            // Unhandled res_code
+            alert("Post: Unhandled res_code");
+          }
+        } else {
+          // TODO handle unable to connect with database
+          alert("Post: unable to connect with database");
+        }
+      }).catch(function (error) {
+          // TODO handle error with the call
+          alert("Post: Call error");
+          console.log(error);
+        });
     }
-    setData([...data, {
-      id: maxID + 1,
-      caption: newCaption,
-      description: newDesc,
-      img: newImg,
-      alt: "Slide_" + (maxID + 1)
-    }]);
+    e.preventDefault();
     //reinitialize
     setNewCaption('');
     setNewDesc('');
-    setNewImg('');
+    setNewImg(null);
 		setCreateModalShow(false);
-		
-		
-
   };
+
   const handleCreateClose = () => {
     setCreateModalShow(false);
   };
@@ -171,9 +269,9 @@ export default function CarouselEdit(prop) {
     setCreateModalShow(true);
   };
 
-
   return (
     <div>
+      {/*{console.log("DATA:",data)}*/}
       <Link style={{fontSize:"17px"}} className="goBack" to="/admin">
         <IoIosArrowBack/>Go Back
       </Link>
--- frontend/src/components/pages/Admin/EditContents/EditCarousel/CreateCarouselForm.js
+++ frontend/src/components/pages/Admin/EditContents/EditCarousel/CreateCarouselForm.js
@@ -6,10 +6,12 @@ import Modal from "react-bootstrap/Modal";
 export default function CarouselForm(prop) {
   const isEditForm = prop.currentItem !== undefined; //true if this form is called when edit button is pressed.
 
-  const captionForm =isEditForm ? <Form.Control onChange={prop.handleCaption} placeholder= {prop.currentItem.caption} /> :
-      <Form.Control onChange={prop.handleCaption} placeholder='Enter Caption' required/>;
+  const captionForm =isEditForm ?
+    <Form.Control onChange={prop.handleCaption} placeholder= {prop.currentItem.caption} /> :
+    <Form.Control onChange={prop.handleCaption} placeholder='Enter Caption' required/>;
 
-  const descForm =isEditForm ? <Form.Control onChange={prop.handleDesc} as="textarea" placeholder= {prop.currentItem.description} /> :
+  const descForm =isEditForm ?
+    <Form.Control onChange={prop.handleDesc} as="textarea" placeholder= {prop.currentItem.description} /> :
     <Form.Control onChange={prop.handleDesc} placeholder='Enter Details' required/>;
 
   return (
@@ -24,7 +26,7 @@ export default function CarouselForm(prop) {
           {descForm}
         </Form.Group>
         <Form.Group controlId="formFileUpload">
-          <Form.File onChange={prop.handleImg} label="Banner Image (e.g., .png/jpeg/jpg files)"/>
+          <Form.File onChange={prop.handleImg} accept=".jpg, .jpeg., .png" label="Banner Image (e.g., .png/jpeg/jpg files) [Optimal Img Size: 2700 x 900 pixels]"/>
         </Form.Group>
         <Modal.Footer>
           <Button onClick={prop.handleClose} buttonColor="msc_orange_invert" buttonSize="btn--medium">Close</Button>
