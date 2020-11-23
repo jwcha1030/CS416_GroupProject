31e3d4a449c312f8d22ad5923911846dde58026b
timkim0713 <timkim0713@gmail.com>
2020-11-24 02:57:58 +0900

    made randomizing order in collections carousel

--- frontend/src/components/collections/CollectionsProductCarousel.js
+++ frontend/src/components/collections/CollectionsProductCarousel.js
@@ -10,41 +10,87 @@ import {
   Slide,
   Slider,
 } from "pure-react-carousel";
-import a1 from "../../images/model_orange.jpg";
-import a2 from "../../images/model_hat.jpg";
-import a3 from "../../images/aboutus.jpg";
-import a4 from "../../images/model_white.jpg";
-import a5 from "../../images/member.jpg";
-import a6 from "../../images/dummy.jpg";
+
+import c0 from "../../images/model_orange.jpg";
+import c1 from "../../images/model_hat.jpg";
+import c2 from "../../images/collections_carousel/c1.jpg";
+import c3 from "../../images/collections_carousel/c2.jpg";
+import c4 from "../../images/collections_carousel/c3.jpg";
+import c5 from "../../images/collections_carousel/c4.jpg";
+import c6 from "../../images/collections_carousel/c5.jpg";
+import c7 from "../../images/collections_carousel/c6.jpg";
+import c8 from "../../images/aboutus.jpg";
+import c9 from "../../images/model_white.jpg";
+import c10 from "../../images/member.jpg";
+
 import s from "./CollectionsProductCarousel.scss";
+
+const NUM_CAROUSEL = 11;
+
+// randomly generated 0-10 unique numbers for randomizing the carousels
+// NOTE: THIS could not be done with loops because of FILE name cannot be changed.
+
+var randomSlideNumber = [];
+while (randomSlideNumber.length < NUM_CAROUSEL) {
+  var r = Math.floor(Math.random() * NUM_CAROUSEL);
+  if (randomSlideNumber.indexOf(r) === -1) randomSlideNumber.push(r);
+}
+var randomOrderImages = [];
+randomOrderImages[randomSlideNumber[0]] = c0;
+randomOrderImages[randomSlideNumber[1]] = c1;
+randomOrderImages[randomSlideNumber[2]] = c2;
+randomOrderImages[randomSlideNumber[3]] = c3;
+randomOrderImages[randomSlideNumber[4]] = c4;
+randomOrderImages[randomSlideNumber[5]] = c5;
+randomOrderImages[randomSlideNumber[6]] = c6;
+randomOrderImages[randomSlideNumber[7]] = c7;
+randomOrderImages[randomSlideNumber[8]] = c8;
+randomOrderImages[randomSlideNumber[9]] = c9;
+randomOrderImages[randomSlideNumber[10]] = c10;
 
 export default () => (
   <CarouselProvider
     visibleSlides={2}
-    totalSlides={6}
-    step={1}
+    totalSlides={NUM_CAROUSEL}
+    step={2}
     naturalSlideWidth={800}
     naturalSlideHeight={1000}
     hasMasterSpinner
+    isPlaying={true}
   >
     <Slider className={s.slider}>
-      <Slide index={0}>
-        <ImageWithZoom src={a1} />
+      <Slide index={randomSlideNumber[0]}>
+        <ImageWithZoom src={randomOrderImages[0]} />
       </Slide>
-      <Slide index={1}>
-        <ImageWithZoom src={a2} />
+      <Slide index={randomSlideNumber[1]}>
+        <ImageWithZoom src={randomOrderImages[1]} />
       </Slide>
-      <Slide index={2}>
-        <ImageWithZoom src={a3} />
+      <Slide index={randomSlideNumber[2]}>
+        <ImageWithZoom src={randomOrderImages[2]} />
       </Slide>
-      <Slide index={3}>
-        <ImageWithZoom src={a4} />
+      <Slide index={randomSlideNumber[3]}>
+        <ImageWithZoom src={randomOrderImages[3]} />
       </Slide>
-      <Slide index={4}>
-        <ImageWithZoom src={a5} />
+      <Slide index={randomSlideNumber[4]}>
+        <ImageWithZoom src={randomOrderImages[4]} />
       </Slide>
-      <Slide index={5}>
-        <ImageWithZoom src={a6} />
+      <Slide index={randomSlideNumber[5]}>
+        <ImageWithZoom src={randomOrderImages[5]} />
+      </Slide>
+      <Slide index={randomSlideNumber[6]}>
+        <ImageWithZoom src={randomOrderImages[6]} />
+      </Slide>
+      <Slide index={randomSlideNumber[7]}>
+        <ImageWithZoom src={randomOrderImages[7]} />
+      </Slide>
+      <Slide index={randomSlideNumber[8]}>
+        <ImageWithZoom src={randomOrderImages[8]} />
+      </Slide>
+      <Slide index={randomSlideNumber[9]}>
+        <ImageWithZoom src={randomOrderImages[9]} />
+      </Slide>
+      <Slide index={randomSlideNumber[10]}>
+        <ImageWithZoom src={randomOrderImages[10]} />
       </Slide>
     </Slider>
     <br />
